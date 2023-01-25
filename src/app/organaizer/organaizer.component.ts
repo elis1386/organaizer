5bb82c5ba2e5a6f18a8edd4e007cc167ad60fc37
elis1386 <elis1386@gmail.com>
2023-01-25 15:40:39 +0200

    add calendar

--- src/app/organaizer/organaizer.component.ts
+++ src/app/organaizer/organaizer.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { DateService } from '../shared/date.service';
 
 @Component({
   selector: 'app-organaizer',
   templateUrl: './organaizer.component.html',
-  styleUrls: ['./organaizer.component.css']
+  styleUrls: ['./organaizer.component.css'],
 })
-export class OrganaizerComponent {
+export class OrganaizerComponent implements OnInit {
+  form!: FormGroup;
 
+  constructor(public dateService: DateService) {}
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl('', Validators.required),
+    });
+  }
+  submit() {
+    const {title} = this.form.value;
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebaseConfig:{
+        apiKey: "AIzaSyB4OY10YaQxRGIYQgP7MSY19JJcgd3J0sA",
+        authDomain: "organaizer-44c64.firebaseapp.com",
+        projectId: "organaizer-44c64",
+        storageBucket: "organaizer-44c64.appspot.com",
+        messagingSenderId: "37140860681",
+        appId: "1:37140860681:web:b22fb07308bcba33694dd8"
+      }
+  };
